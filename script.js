a6586876ccd2b1c482d2691ea03db4e4b3f4d22f
Mahansa <hansagamers11@gmail.com>
2025-05-26 20:35:20 +0700

    update pada Buy weapon dan persiapan sell weapon

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-  if (currentWeaponIndex < weapons.length) {
+  if (currentWeaponIndex < weapons.length - 1) {
     if (gold >= 30) {
       gold -= 30;
       currentWeaponIndex++;
@@ -98,9 +98,16 @@ function buyWeapon() {
     } else {
       text.innerText = "You do not have enough gold to buy a weapon.";
     }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
   }
 }
 
+function sellWeapon() {
+
+}
 
 function fightSlime() {
 
